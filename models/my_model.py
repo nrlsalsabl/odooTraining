f38e21b69612f3bd9b3f6ead1c9cb3b12531196a
nrlsalsabl <nurulsalsabila315@gmail.com>
2024-10-25 11:38:15 +0700

    first tester

---
+++ models/my_model.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api # type: ignore
+
+class MyModel(models.Model):
+    _name = 'my_module.my_model'
+    _description = 'Simple model example'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
